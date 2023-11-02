c7a77d2cb37e1061e3e51ce6215d772075033c20
brand-steps <bhaia9036@gmail.com>
2023-11-02 16:58:06 +0500

    first commit

--- index.js
+++ index.js
@@ -18,18 +18,14 @@ import { requestModel } from "./Models/User.js";
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-/*
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "*"],
-    credentials: true,
-  })
-);*/
-app.use(
-  cors({
-    origin: "https://www.equipmentsuppliers.co.uk",
+    origin: ["https://www.equipmentsuppliers.co.uk", "*"],
+    //credentials: true,
   })
 );
+
 const storage = multer.diskStorage({
   destination: "/tmp",
   filename: function (req, file, cb) {
